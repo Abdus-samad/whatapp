38c75ca37c322052b9d7f09fb70b953e98d18619
Abdus-samad <abdussamadajao@gmail.com>
2021-05-31 13:37:55 +0100

    first commit

---
+++ firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBEMhNROThE_oT-o459f2a1klCKfT97l0U',
+  authDomain: 'whatapp-2-cc429.firebaseapp.com',
+  projectId: 'whatapp-2-cc429',
+  storageBucket: 'whatapp-2-cc429.appspot.com',
+  messagingSenderId: '732379374938',
+  appId: '1:732379374938:web:fa96e058c410fb44d5bf63',
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+
+const db = app.firestore()
+const auth = app.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+
+export { db, auth, provider}
