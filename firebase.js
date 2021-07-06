1d53ec72baf8ddf69c75c4324e3e2981ad9543f9
Abdussamad ajao <63202523+Abdus-samad@users.noreply.github.com>
2021-07-06 06:20:09 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBEMhNROThE_oT-o459f2a1klCKfT97l0U',
-  authDomain: 'whatapp-2-cc429.firebaseapp.com',
-  projectId: 'whatapp-2-cc429',
-  storageBucket: 'whatapp-2-cc429.appspot.com',
-  messagingSenderId: '732379374938',
-  appId: '1:732379374938:web:fa96e058c410fb44d5bf63',
+  apiKey: "AIzaSyBEMhNROThE_oT-o459f2a1klCKfT97l0U",
+  authDomain: "whatapp-2-cc429.firebaseapp.com",
+  projectId: "whatapp-2-cc429",
+  storageBucket: "whatapp-2-cc429.appspot.com",
+  messagingSenderId: "732379374938",
+  appId: "1:732379374938:web:8447ae3b8a2d4613d5bf63"
 };
-
 const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
   : firebase.app();
